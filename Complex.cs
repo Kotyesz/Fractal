d60b99c773c66f4b264ed5ff009abae8b972a637
Farkas Martin <47693688+Kotyesz@users.noreply.github.com>
2020-01-19 15:34:38 +0000

    Program

---
+++ Complex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractal
+{
+    public class Complex
+    {
+        public double a, b, temp;
+
+        public Complex(double a,double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Square()
+        {
+            temp = (a * a) - (b * b);
+            b = 2.0 * a * b;
+            a = temp;
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt((a * a) + (b * b));
+        }
+
+        public void Add(Complex c)
+        {
+            a += c.a;
+            b += c.b;
+        }
+
+    }
+}
